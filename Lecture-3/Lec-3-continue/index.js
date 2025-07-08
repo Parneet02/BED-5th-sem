4fc6b1db37ba48c3302bee317ee13d2c014534c1
Parneet02 <baaniparneet0208@gmail.com>
2025-07-08 12:56:20 +0530

    Lecture-3

---
+++ Lecture-3/Lec-3-continue/index.js
@@ -0,0 +1,2 @@
+let mypackage=require("sumpackage-parneet2412")
+console.log(mypackage.sum(2,3))
---
+++ Lecture-3/lib/sum.js
@@ -0,0 +1,6 @@
+//how to publish code
+function sum(a,b){
+    return a+b;
+}
+
+module.exports.sum=sum
