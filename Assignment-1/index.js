855d4137a9bc2bb499bae76e13cbdd94cb236c40
Parneet02 <baaniparneet0208@gmail.com>
2025-07-17 14:10:11 +0530

    Assignment-1

---
+++ Assignment-1/index.js
@@ -0,0 +1,7 @@
+const fs=require('fs');
+let file=process.argv.splice(2).join(" ");
+
+fs.writeFile("./demo.txt",file,function(err){
+    if(err) return console.log(err);
+    console.log("success!!");
+})
--- Lecture-3/index.js
+++ Lecture-3/index.js
@@ -1,3 +1,3 @@
 const express = require("express") //to require packages, if we use node modules packages we have to give only name and if we using any ither then write full path
-console.log(express)
+//console.log(express)
 module.exports=require("./lib/sum")
